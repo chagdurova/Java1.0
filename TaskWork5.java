8f895c822f95b10d74ac56a15b3bcd47614e5adc
Дарья <baligima.chagdurova@gmail.com>
2021-07-31 03:22:52 +0300

    Задание 5

---
+++ TaskWork5.java
@@ -0,0 +1,41 @@
+package com.Dasha.idea;
+import java.util.Scanner;
+
+/*
+Ввести 2 числа с клавиатуры, затем один из символов ‘+’, ‘-’, ‘*’ или ‘/’.
+В зависимости от символа вывести в консоль результат выполнения соответствующей арифметической операции.
+ */
+
+public class TAskWork5 {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result;
+        char operation;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("ВВедите два числа: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("Введите оператор:  ");
+        op = reader.next().charAt(0);
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.printf("не верный оператор");
+                return;
+        }
+        System.out.printf("Ответ: " + result);
+    }
+}
+
