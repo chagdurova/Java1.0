a8d3052e5eb71fb3747c5d429102f41b407f7eef
Дарья <baligima.chagdurova@gmail.com>
2021-08-10 00:31:49 +0300

    исправленное задание 3

--- TAskWork03.java
+++ TAskWork03.java
@@ -1,21 +1,22 @@
 package com.Dasha.idea;
+
+import java.util.Arrays;
 /*
 Задайте массив из 5 любых целых чисел.
 Поменяйте местами первый и последний элемент в массиве.
 Вывести в консоль результат суммы первого и среднего элемента.
  */
 
-public class TAskWork03 {
-    public static void main(String[] args){
-        int[] number =  {1,  10, 67, 434, 7};
-        System.out.println("Начальный Массив = " + number);
+public class TaskWork03 {
+    public static void main(String[] args) {
+        int[] number = {1, 10, 67, 434, 7};
+        System.out.println("Начальный Массив = " + Arrays.toString(number));
 
         System.out.println("Первый элемент массива =" + number[0]);
         System.out.println("Последний элемент массива =" + number[4]);
 
-        int firstelement = number[0];
-        number[0] = number[number.length-1];
-        number[number.length-1] = firstelement;
-        System.out.println("Новый Массив = " + number);
+        int average = number[(number.length - 1) / 2];
+        int summ = average + number[0];
+        System.out.printf("сумма первого и среднего элемента = %d", summ);
     }
 }
